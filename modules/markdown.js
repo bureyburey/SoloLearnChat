974cb289909826d8833c1fd7badb75fbb8e1457c
Michael Ermishin <ermichin1999@gmail.com>
2017-01-05 23:42:01 +0300

    Update markdown.js

--- modules/markdown.js
+++ modules/markdown.js
@@ -1,28 +1,46 @@
-function textToHtml(text) {
-	text = escapeHtml(text);
-	text = text.replace(/\n/g, '<br>');
+var markdown = {
+	htmlEntitiesMap: {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#039;',
+		' ': '&nbsp;'
+	},
+		
+	textToHtml: function(text) {
+		text = this.specToEntities(text);
+		text = text.replace(/\n/g, '<br>');
+		
+		text = text.replace(
+			/([^\\]|^)\[url:([^\]]+)\]\(([^)]+)\)/g,
+			(_, smb, name, url) => smb + '<a href="' + url + '">' + name + '</a>'
+		);
+		
+		text = text.replace(/([^\\]|^)\*\*(.+?)\*\*/g, (_, smb, content) => smb + '<b>' + content + '</b>');
+		text = text.replace(/([^\\]|^)__(.+?)__/g, (_, smb, content) => smb + '<i>' + content + '</i>');
+		
+		text = text.replace(/\\(.)/g, (_, character) => character);
+		
+		return text;
+	},
 	
-	text = text.replace(
-		/([^\\]|^)\[url:([^\]]+)\]\(([^)]+)\)/g,
-		(_, smb, name, url) => smb + '<a href="' + url + '">' + name + '</a>'
-	);
+	specToEntities: function(text) {
+		var pattern = new RegExp('/[' + Object.keys(this.htmlEntitiesMap).join('') + ']/', 'g');
+		return text.replace(pattern, k => this.htmlEntitiesMap[k]);
+	},
 	
-	text = text.replace(/([^\\]|^)\*\*(.+?)\*\*/g, (_, smb, content) => smb + '<b>' + content + '</b>');
-	text = text.replace(/([^\\]|^)__(.+?)__/g, (_, smb, content) => smb + '<i>' + content + '</i>');
+	entitiesToSpec: function(text) {
+		var entToSpecMap = Object.keys(this.htmlEntitiesMap).reduce(function(obj, key){
+			obj[data[key]] = key;
+			return obj;
+		}, {});
+		
+		var pattern = new RegExp('/[' + Object.keys(entToSpecMap).join('') + ']/', 'g');
+		return text.replace(pattern, k => entToSpecMap[k]);
+	},
 	
-	text = text.replace(/\\(.)/g, (_, character) => character);
-	
-	return text;
-}
-
-function escapeHtml(text) {
-  var map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#039;'
-  };
-
-  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-}
+	htmlToText: function(html) {
+		return html.replace(/<.*?>/g, '');
+	}
+};
