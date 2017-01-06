cc7684b6766698ae13ff4d55fa003ef1a6190fec
Michael Ermishin <ermichin1999@gmail.com>
2017-01-06 17:31:32 +0300

    Update API.js

--- modules/API.js
+++ modules/API.js
@@ -1,18 +1,23 @@
 var API = {
-	messages: [],
-	users: [],
+	_messages: [],
+	_users: [],
 
 	_message_updates_listeners: [],
 	_user_updates_listeners: [],
 
-	getActiveUsers: () => this.users,
-	getMessageList: () => this.messages, 
+	MODE_REGISTER: 'register',
+	MODE_LOGIN: 'login',
+
+	getActiveUsers: function() { return this._users; },
+	getMessageList: function() { return this._messages; },
+
+	logIn: (user, mode) => chat.login(user.name, user.password, mode),
 
 	sendMessage: message => chat.sendMessage(message),
 	updateMessage: (id, message) => chat.updateMessage(id, message),
 
 	_updateMessages: function(new_messages) {
-		this.messages = this.messages.concat(new_messages);
+		this._messages = this._messages.concat(new_messages);
 
 		// Call update for each message on each listener
 		for (var listenerId = 0; listenerId < this._message_updates_listeners.length; ++listenerId)
@@ -21,13 +26,13 @@ var API = {
 	},
 
 	_updateConnectedUsers: function(users) {
-		var users_joined = users.filter(user => this.users.indexOf(user) === -1);
-		var users_quited = this.users.filter(user => users.indexOf(user) === -1);
+		var users_joined = users.filter(user => this._users.indexOf(user) === -1);
+		var users_quited = this._users.filter(user => users.indexOf(user) === -1);
 
 		var user_actions = users_joined.map(user => ({ 'name': user.name, 'action': 'join' }));
 		user_actions = user_actions.concat(users_quited.map(user => ({ 'name': user.name, 'action': 'exit' })));
 
-		this.users = users;
+		this._users = users;
 
 		for (var listenerId = 0; listenerId < this._user_updates_listeners.length; ++listenerId)
 			for (var actId = 0; actId < user_actions.length; ++actId)
